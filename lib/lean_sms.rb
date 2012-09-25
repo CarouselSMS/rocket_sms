b9e0c9982d39b3cda593916ba12cb125d37e1208
Marcelo Wiermann <marcelo.wiermann@gmail.com>
2012-09-25 00:41:07 -0300

    basic structure

--- lib/lean_sms.rb
+++ lib/lean_sms.rb
@@ -1,5 +1,29 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'eventmachine'
+require 'smpp'
+require 'em-hiredis'
+require 'oj'
+require 'multi_json'
+require 'singleton'
+require 'securerandom'
+
 require "lean_sms/version"
 
-module LeanSms
-  # Your code goes here...
+module LeanSMS
+
+  # Disable ruby-smpp logging
+  require 'tempfile'
+  Smpp::Base.logger = Logger.new(Tempfile.new('ruby-smpp').path)
+
+  LIB_PATH = File.dirname(__FILE__) + '/lean_sms/'
+
+  %w{ gateway did message transceiver scheduler configurator }.each do |dep|
+    require LIB_PATH + dep
+  end
+
+  def self.configure
+    yield self
+  end
+
 end
