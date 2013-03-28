c6ca4f54a82efbfc787796d04b94c7e242596742
Alex Gusev <hello.from.code@gmail.com>
2013-03-28 00:58:24 -0400

    better usage example

--- examples/gateway.rb
+++ examples/gateway.rb
@@ -1,7 +1,20 @@
 require 'rocket_sms'
 
+path = File.dirname(File.expand_path(__FILE__))
+
+Process.daemon(true) if ARGV[0]
+pid = Process.pid
+
+FileUtils.mkdir_p "#{path}/tmp/pids"
+FileUtils.mkdir_p "#{path}/logs"
+
+IO.write("#{path}/tmp/pids/gateway.pid", pid)
+
+environment = ARGV[0] || "development"
+
 RocketSMS.configure do |config|
-  config.settings = 'examples/gateway.yml'
+  config.settings = '#{path}/gateway.yml'
+  config.log_location = "#{path}/logs/gateway.log"
 end
 
 RocketSMS.start
