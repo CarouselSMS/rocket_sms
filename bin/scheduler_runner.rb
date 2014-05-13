1f30d3a80b0ab26c5b3143abd9dbb82ec41d16b3
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2014-05-13 16:42:06 +0300

    Sync!

--- bin/scheduler_runner.rb
+++ bin/scheduler_runner.rb
@@ -4,6 +4,7 @@ require "rocket_sms"
 
 redis_url = ENV['REDIS_URL'] || ARGV[0]
 log_location = ENV['LOG_LOCATION'] || ARGV[1] || STDOUT
+log_location.sync = true if log_location.respond_to? :sync=
 log_level = ENV['LOG_LEVEL'].to_i || Logger::INFO
 
 scheduler = RocketSMS::Scheduler.instance
--- bin/transceiver_runner.rb
+++ bin/transceiver_runner.rb
@@ -5,6 +5,7 @@ require "rocket_sms"
 id = ENV['TRANSCEIVER_ID'] || ARGV[0]
 redis_url = ENV['REDIS_URL'] || ARGV[1]
 log_location = ENV['LOG_LOCATION'] || ARGV[2] || STDOUT
+log_location.sync = true if log_location.respond_to? :sync=
 log_level = ENV['LOG_LEVEL'].to_i || Logger::INFO
 
 transceiver = RocketSMS::Transceiver.new(id, redis_url, log_location, log_level)
