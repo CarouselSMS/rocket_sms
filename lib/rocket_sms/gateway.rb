bffff33a1e6392d3b0e59cadc15b92d81abd025f
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2014-06-16 16:56:49 +0300

    Cleanup.

--- lib/rocket_sms/gateway.rb
+++ lib/rocket_sms/gateway.rb
@@ -107,14 +107,23 @@ module RocketSMS
 
     def start_scheduler
       cmd = "bundle exec ruby #{@path}/bin/scheduler_runner.rb"
-      @scheduler[:pid] = Process.spawn({ "REDIS_URL" => redis_url, "LOG_LOCATION" => (log_location == STDOUT ? nil : log_location), "LOG_LEVEL" => log_level.to_s }, cmd)
+      @scheduler[:pid] = Process.spawn({
+        "REDIS_URL" => redis_url,
+        "LOG_LOCATION" => (log_location == STDOUT ? nil : log_location),
+        "LOG_LEVEL" => log_level.to_s
+      }, cmd)
     end
 
     def start_transceivers
       settings[:transceivers].each do |tid, settings|
         cmd = "bundle exec ruby #{@path}/bin/transceiver_runner.rb"
         @transceivers[tid] = {}
-        @transceivers[tid][:pid] = Process.spawn({ "TRANSCEIVER_ID" => tid.to_s ,"REDIS_URL" => redis_url, "LOG_LOCATION" => (log_location == STDOUT ? nil : log_location), "LOG_LEVEL" => log_level.to_s }, cmd)
+        @transceivers[tid][:pid] = Process.spawn({
+          "TRANSCEIVER_ID" => tid.to_s,
+          "REDIS_URL" => redis_url,
+          "LOG_LOCATION" => (log_location == STDOUT ? nil : log_location),
+          "LOG_LEVEL" => log_level.to_s
+        }, cmd)
       end
     end
 
